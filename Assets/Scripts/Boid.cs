e80890a82d13003842554bdb6fe7305ff7b4933c
Chui <lordbabypanda@gmail.com>
2016-01-08 16:28:12 -0600

    manual merge

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -2,36 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 
-<<<<<<< HEAD
-public class Boid : MonoBehaviour 
-=======
-public class Boid : MonoBehaviour
->>>>>>> origin/master
-{
 
+public class Boid : MonoBehaviour
+{
     public Vector3 Vel;
 
-<<<<<<< HEAD
-
-    float Mag(Vector3 pos1) // Magitude
-    {
-        return Mathf.Sqrt((pos1.x) * (pos1.x) + (pos1.y) * (pos1.y) + (pos1.z) * (pos1.z)); // Direction = Vector / mag
-    }
-
-
-    void Start()
-    {
-        //Vel = new Vector3(1f, 1f, 1f); // Sets all boids start Speed;
-    }
-
-
-=======
     float Mag(Vector3 pos1) // Magitude
     {
         return Mathf.Sqrt((pos1.x) * (pos1.x) + (pos1.y) * (pos1.y) + (pos1.z) * (pos1.z)); // Direction = Vector / mag // Size == mag = vector3 / direction
     }
 
->>>>>>> origin/master
     void Update()
     {
         transform.right = Vel.normalized;
@@ -41,11 +21,4 @@ public class Boid : MonoBehaviour
             Application.Quit();
         }
     }
-<<<<<<< HEAD
-
-  
-
-
-=======
->>>>>>> origin/master
 }
--- Assets/Scripts/BoidController.cs
+++ Assets/Scripts/BoidController.cs
@@ -3,11 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-<<<<<<< HEAD
-public class BoidController : MonoBehaviour 
-=======
+
 public class BoidController : MonoBehaviour
->>>>>>> origin/master
 {
     public List<GameObject> boids = new List<GameObject>();
     public Slider cohesionslider;
@@ -19,19 +16,12 @@ public class BoidController : MonoBehaviour
     public float cFac, sFac, aFac, vLim;
     public int Xmin = 0, Ymin = 0, Zmin = 0, Xmax = 0, Ymax = 0, Zmax = 0; // Draws Box
 
-<<<<<<< HEAD
-    Vector3 v1 = Vector3.zero;
-    Vector3 v2 = Vector3.zero;
-    Vector3 v3 = Vector3.zero;
-    Vector3 v4 = Vector3.zero;
-=======
     Vector3 v1;
     Vector3 v2;
     Vector3 v3;
     Vector3 v4;
     Vector3 center;
     Vector3 pv;
->>>>>>> origin/master
 
     /// <summary>
     /// create the list of boids in random positions
@@ -43,15 +33,9 @@ public class BoidController : MonoBehaviour
         for (int i = 0; i < 25; i++) // Control how many number of boids on Screen
         {
             bird = Instantiate(boid) as GameObject;
-<<<<<<< HEAD
             float rX = Random.Range(-10, 10);
             float rY = Random.Range(-10, 10);
             float rZ = Random.Range(-10, 10);
-=======
-            float rX = Random.Range(0, 20);
-            float rY = Random.Range(0, 20);
-            float rZ = Random.Range(0, 20);
->>>>>>> origin/master
             bird.transform.position = new Vector3(rX, rY, rZ);
             boids.Add(bird);
         }
@@ -60,11 +44,8 @@ public class BoidController : MonoBehaviour
     [ContextMenu("Unpopulate")] // Matthew's code
     void UnPopulate()
     {
-<<<<<<< HEAD
-        foreach(GameObject go in boids)
-=======
+
         foreach (GameObject go in boids)
->>>>>>> origin/master
         {
             DestroyImmediate(go);
         }
@@ -73,18 +54,6 @@ public class BoidController : MonoBehaviour
 
     void Start()
     {
-<<<<<<< HEAD
-           GameObject bird;
-           for (int i = 0; i < 25; i++) // Control how many number of boids on Screen
-           {
-               bird = Instantiate(boid) as GameObject;
-               float rX = Random.Range(0, 10);
-               float rY = Random.Range(0, 10);
-               float rZ = Random.Range(0, 10);
-               bird.transform.position = new Vector3(rX, rY, rZ);
-               boids.Add(bird);
-           }
-=======
         GameObject bird;
         for (int i = 0; i < 25; i++) // Control how many number of boids on Screen
         {
@@ -95,22 +64,10 @@ public class BoidController : MonoBehaviour
             bird.transform.position = new Vector3(rX, rY, rZ);
             boids.Add(bird);
         }
->>>>>>> origin/master
     }
 
     void Update()
     {
-<<<<<<< HEAD
-        foreach (GameObject cookies in boids)
-        {
-            v1 = rule1(cookies) * cohesionslider.value * cFac; // Cohesion
-            v2 = rule2(cookies) * separtionslider.value * sFac;
-            v3 = rule3(cookies) * alignmentslider.value * aFac;
-            v4 = boundingBox(cookies);
-            cookies.GetComponent<Boid>().Vel = cookies.GetComponent<Boid>().Vel + v1 + v2 + v3 + v4; // Vel + all rules
-            l_Vel(cookies);
-            cookies.transform.position = cookies.transform.position + cookies.GetComponent<Boid>().Vel; // Adding the pos + Vel
-=======
         foreach (GameObject boid in boids)
         {
             v1 = rule1(boid) * cohesionslider.value * cFac; // Cohesion
@@ -120,7 +77,6 @@ public class BoidController : MonoBehaviour
             boid.GetComponent<Boid>().Vel = boid.GetComponent<Boid>().Vel + v1 + v2 + v3 + v4; // Vel + all rules
             l_Vel(boid);
             boid.transform.position = boid.transform.position + boid.GetComponent<Boid>().Vel; // Adding the pos + Vel
->>>>>>> origin/master
         }
         if (Input.GetKey("escape"))
         {
@@ -130,55 +86,16 @@ public class BoidController : MonoBehaviour
 
     void l_Vel(GameObject lv) // Velocity Limit
     {
-<<<<<<< HEAD
-        Vector3 panda = lv.GetComponent<Boid>().Vel;
-        if (panda.magnitude > vLim)
-        { 
-            panda = panda.normalized * vLim;
-=======
         Vector3 velocity = lv.GetComponent<Boid>().Vel;
         if (velocity.magnitude > vLim)
         {
             velocity = velocity.normalized * vLim;
->>>>>>> origin/master
         }
     }
 
-
-<<<<<<< HEAD
-    Vector3 boundingBox(GameObject bird) // Of the box, it will use force to push back boids.
-    {
-        Vector3 v = Vector3.zero;
-        
-
-        if (bird.transform.position.x < Xmin)
-        {
-            v.x =  Xmin - bird.transform.position.x;
-        }
-        else if (bird.transform.position.x > Xmax)
-        {
-            v.x = Xmax - bird.transform.position.x;
-        }
-        if (bird.transform.position.y < Ymin)
-        {
-            v.y = Ymin - bird.transform.position.y;
-        }
-        else if (bird.transform.position.y > Ymax)
-        {
-            v.y = Ymax - bird.transform.position.y;
-        }
-        if (bird.transform.position.z < Zmin)
-        {
-            v.z = Zmin - bird.transform.position.z;
-        }
-        else if (bird.transform.position.z > Zmax)
-        {
-            v.z = Zmax - bird.transform.position.z;
-=======
     Vector3 boundingBox(GameObject boid) // Of the box, it will use force to push back boids.
     {
         Vector3 v = Vector3.zero;
-
 
         if (boid.transform.position.x < Xmin)
         {
@@ -203,7 +120,6 @@ public class BoidController : MonoBehaviour
         else if (boid.transform.position.z > Zmax)
         {
             v.z = Zmax - boid.transform.position.z;
->>>>>>> origin/master
         }
         return v;
     }
@@ -216,11 +132,9 @@ public class BoidController : MonoBehaviour
 
     public Vector3 rule1(GameObject Cohesion) // Rule1  = COHESION!
     {
-<<<<<<< HEAD
         Vector3 center = Vector3.zero; // Center of mass
-=======
+
         // Center of mass
->>>>>>> origin/master
         if (target.isOn)
         {
             return (Target.transform.position - Cohesion.transform.position) / 100; // Moving 1% of boids to Target
@@ -236,21 +150,12 @@ public class BoidController : MonoBehaviour
         return (center - Cohesion.transform.position) / 100; // Cohesion, Moving 1%
     }
 
-<<<<<<< HEAD
-   public Vector3 rule2(GameObject separtion)
-    {
-        Vector3 center = new Vector3(0,0,0);
-        foreach (GameObject currentboid in boids)
-        {
-            if(currentboid != separtion)
-=======
     public Vector3 rule2(GameObject separtion)
     {
         Vector3 center = new Vector3(0, 0, 0);
         foreach (GameObject currentboid in boids)
         {
             if (currentboid != separtion)
->>>>>>> origin/master
             {
                 if (Distance(currentboid.transform.position, separtion.transform.position) < 7) // Set Distance away from each other
                 {
@@ -263,24 +168,14 @@ public class BoidController : MonoBehaviour
 
     public Vector3 rule3(GameObject Aligment)
     {
-<<<<<<< HEAD
         Vector3 pv = Vector3.zero;
-=======
 
->>>>>>> origin/master
         foreach (GameObject currentboid in boids)
         {
             if (currentboid != Aligment)
                 pv += currentboid.GetComponent<Boid>().Vel;
         }
         pv = pv / (boids.Count - 1); // Center of Mass
-<<<<<<< HEAD
         return (pv - Aligment.GetComponent<Boid>().Vel) / 1; // if / by 8 is 1/8, if 100, it's 1/100. 1 is GREAT! 
     }
-
-  
-=======
-        return (pv - Aligment.GetComponent<Boid>().Vel) / 1; // if / by 8 is 1/8, if 100, it's 1/100. 1 is GREAT!
-    }
->>>>>>> origin/master
 }
